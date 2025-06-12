80be45a50896ddef411e84e9b74dc8b6c0344f5b
yinquest11 <yinquest11@gmail.com>
2025-06-12 17:52:19 +0800

    Throw and swing enemy mechanics

---
+++ Assets/TestingScript/TestAddForceToBlock.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class TestAddForceToBlock : MonoBehaviour
+{
+
+    public Rigidbody2D _block;
+    private DistanceJoint2D _distanceJoint;
+
+    public float forceAmount = 200f;
+    public Vector2 forceDirection;
+
+    Vector2 _playerDirectionToBlock;
+    Vector2 _blockPrependicularDirectionToPlayer;
+
+    private bool isReverse = false;
+
+
+
+
+
+
+    private void Start()
+    {
+
+        _distanceJoint = GetComponentInChildren<DistanceJoint2D>();
+        _block.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+    }
+
+
+
+    
+    void Update()
+    {
+        
+        _playerDirectionToBlock = (_block.transform.position - gameObject.transform.position).normalized;
+        _blockPrependicularDirectionToPlayer =  Vector2.Perpendicular(_playerDirectionToBlock);
+
+        
+
+        Debug.DrawRay(_block.transform.position, _blockPrependicularDirectionToPlayer, Color.green);
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            // use isReverse to determine want to use the opposite direction or not ?
+            isReverse = !isReverse;
+
+            forceDirection = isReverse ? -_blockPrependicularDirectionToPlayer: _blockPrependicularDirectionToPlayer;
+
+
+
+            _block.linearVelocity = Vector2.zero;
+            _block.AddForce(forceDirection * forceAmount, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+
+            _distanceJoint.enabled = !_distanceJoint.enabled;
+            
+        }
+
+
+
+    }
+}
